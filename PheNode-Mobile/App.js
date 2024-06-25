35292a9b3cdef168b421b166b9cf890d8d2b4aa1
JaccobStanton <jstanton23@my.stlcc.edu>
2024-06-25 00:16:02 -0500

    Added PheNode listing page

--- PheNode-Mobile/App.js
+++ PheNode-Mobile/App.js
@@ -1,9 +1,9 @@
 import * as React from "react";
-import { View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "./screens/login/LoginScreen";
 import SignupScreen from "./screens/signup/SignupScreen";
+import LandingPage from "./screens/LandingPage";
 
 const Stack = createNativeStackNavigator();
 
@@ -17,6 +17,7 @@ function App() {
       >
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="SignUp" component={SignupScreen} />
+        <Stack.Screen name="LandingPage" component={LandingPage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
